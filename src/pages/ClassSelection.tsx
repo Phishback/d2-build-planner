66485f8d3f45082e5ef1cd00b738d851577510d8
Phishback <fishback.evan@gmail.com>
2026-02-21 10:40:35 -0500

    add class selection component and prettier

---
+++ src/pages/ClassSelection.tsx
@@ -0,0 +1,18 @@
+import ClassesList from "../data/classes";
+
+const ClassSelection = () => {
+  return (
+    <div>
+      {Object.values(ClassesList).map((characterClass) => (
+        <button
+          key={characterClass.name}
+          onClick={() => console.log(characterClass)}
+        >
+          {characterClass.name}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default ClassSelection
